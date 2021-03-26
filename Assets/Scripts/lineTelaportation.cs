a3eb940d6e277f4d6a2c9e4c6f7ec24bcc6210e5
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-03-25 17:38:55 -0700

    added colliders to lines

--- Assets/Scripts/lineTelaportation.cs
+++ Assets/Scripts/lineTelaportation.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class lineTelaportation : MonoBehaviour
 {
     public List<GameObject> Lines;
-    public List<Vector3> LinePositions;
     public GameObject electron;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach(GameObject line in Lines)
-        {
-            var lineposition = line.transform.position;
-            LinePositions.Add(lineposition);
-        }
+        
     }
 
     // Update is called once per frame
@@ -24,13 +19,18 @@ public class lineTelaportation : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
+            UnityEngine.Debug.Log("pressed");
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            int layerMask = 1 << 8;
+          //  int layerMask = 1 << 8;
 
-            if(Physics.Raycast(ray, out hit, 1000, layerMask))
+            if(Physics.Raycast(ray, out hit, 1000))
             {
-                electron.transform.position = hit.transform.position;
+                UnityEngine.Debug.Log("hit");
+                if (Lines.Contains(hit.collider.gameObject))
+                {
+                    electron.transform.position = hit.collider.gameObject.transform.position;
+                }
             }
         }
     }
