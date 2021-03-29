36ac75ac39a6b7ac5a21dbb6f585f5b400c0c5ba
clairetigner <claire.tigner@gmail.com>
2021-03-29 15:24:52 -0700

    Cards Button

---
+++ Assets/Scripts/CardButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardButton : MonoBehaviour
+{
+
+    public List<GameObject> Cards;
+    public void FlipCard()
+    {
+        this.GetComponent<Animation>().Play("yuhh");
+        Card.removeAt(0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
