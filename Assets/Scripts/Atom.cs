8188613f58610b629990b6d141ac893ad32596af
MarcEftimie <53620308+MarcEftimie@users.noreply.github.com>
2021-04-01 16:58:46 -0700

    More code

--- Assets/Scripts/Atom.cs
+++ Assets/Scripts/Atom.cs
@@ -8,12 +8,11 @@ public class Element : MonoBehaviour
     // Defines Name of atom and the kJValues for the colorSet that the element emits
     public string name;
     public List<string> colors;
-    public List<float> kJValues;
     public List<float> linePositions;
+    public List<int> kJValues;
 
-    
     // Defines the Element Object
-    public Element(string Name, List<string> colorSet, List<float> lines, List<float> kJ)
+    public Element(string Name, List<string> colorSet, List<float> lines, List<int> kJ)
     {
         name = Name;
         linePositions = lines;
