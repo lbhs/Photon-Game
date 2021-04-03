31703f2ea20a7ba981904fb3e6659e18f1124ec8
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-04-02 21:14:10 -0700

    idk

--- Assets/Scripts/ElementCreator.cs
+++ Assets/Scripts/ElementCreator.cs
@@ -4,68 +4,28 @@ using UnityEngine;
 
 public class ElementCreator : MonoBehaviour
 {
-    public List<Color> Colors;
-    public float[,] kJvalues;
     public List<Color> Completed;
     public List<string> Combinations;
-    public List<int> RedkJ;
-    public List<int> OrangekJ;
-    public List<int> YellowkJ;
-    public List<int> GreenkJ;
-    public List<int> CyankJ;
-    public List<int> BluekJ;
-    public List<int> VioletkJ;
 
     void Start()
     {
-        for (int i = 158; i < 190; i++)
-        {
-            RedkJ.Add(i);
-        }
-        for (int i = 190; i < 201; i++)
-        {
-            OrangekJ.Add(i);
-        }
-        for (int i = 201; i < 210; i++)
-        {
-            YellowkJ.Add(i);
-        }
-        for (int i = 210; i < 238; i++)
-        {
-            GreenkJ.Add(i);
-        }
-        for (int i = 238; i < 245; i++)
-        {
-            CyankJ.Add(i);
-        }
-        for (int i = 245; i < 264; i++)
-        {
-            BluekJ.Add(i);
-        }
-        for (int i = 264; i < 312; i++)
-        {
-            VioletkJ.Add(i);
-        }
         StartCoroutine("CreateElement");
     }
 
     IEnumerator CreateElement()
     {
-        for(int x1 = 0; x1 < 313; x1++)
+        while (Completed.Count < 3)
         {
-            for(int x2 = 0; x2 < x1; x2++)
+            var x1 = Random.Range(0, 313);
+            var x2 = Random.Range(0, 313);
+            var x3 = Random.Range(0, 313);
+            if (CheckNumbers(x1, x2, x3))
             {
-                for(int x3 = 0; x3 < x2; x3++)
-                {
-                    if (CheckNumbers(x1, x2, x3))
-                    {
-                        StopCoroutine("CreateElement");
-                    }
-                    else
-                    {
-                        print(x1 + ", " + x2 + ", " + x3);
-                    }
-                }
+                print("Found Element");
+            }
+            else
+            {
+                print(x1 + ", " + x2 + ", " + x3);
             }
         }
         yield return new WaitForSeconds(0.0f);
@@ -77,51 +37,57 @@ public class ElementCreator : MonoBehaviour
         Combinations.Clear();
         while (Completed.Count < 3)
         {
-                if (RedkJ.Contains(x1) || OrangekJ.Contains(x1) || YellowkJ.Contains(x1) || GreenkJ.Contains(x1) || CyankJ.Contains(x1) || BluekJ.Contains(x1) || VioletkJ.Contains(x1))
+                if (CheckColors(x1) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x1);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x1");
                     }
                 }
-                if (RedkJ.Contains(x2) || OrangekJ.Contains(x2) || YellowkJ.Contains(x2) || GreenkJ.Contains(x2) || CyankJ.Contains(x2) || BluekJ.Contains(x2) || VioletkJ.Contains(x2))
+                if (CheckColors(x2) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x2);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x2");
                     }
                 }
-                if (RedkJ.Contains(x1 + x2) || OrangekJ.Contains(x1 + x2) || YellowkJ.Contains(x1 + x2) || GreenkJ.Contains(x1 + x2) || CyankJ.Contains(x1 + x2) || BluekJ.Contains(x1 + x2) || VioletkJ.Contains(x1 + x2))
+                if (CheckColors(x1 + x2) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x1 + x2);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x3");
                     }
                 }
-                if (RedkJ.Contains(x2 + x3) || OrangekJ.Contains(x2 + x3) || YellowkJ.Contains(x2 + x3) || GreenkJ.Contains(x2 + x3) || CyankJ.Contains(x2 + x3) || BluekJ.Contains(x2 + x3) || VioletkJ.Contains(x2 + x3))
+                if (CheckColors(x3 + x2) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x3 + x2);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x1 + x2");
                     }
                 }
-                if (RedkJ.Contains(x3) || OrangekJ.Contains(x3) || YellowkJ.Contains(x3) || GreenkJ.Contains(x3) || CyankJ.Contains(x3) || BluekJ.Contains(x3) || VioletkJ.Contains(x3))
+                if (CheckColors(x3) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x3);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x2 + x3");
                     }
                 }
-                if (RedkJ.Contains(x1 + x2 + x3) || OrangekJ.Contains(x1 + x2 + x3) || YellowkJ.Contains(x1 + x2 + x3) || GreenkJ.Contains(x1 + x2 + x3) || CyankJ.Contains(x1 + x2 + x3) || BluekJ.Contains(x1 + x2 + x3) || VioletkJ.Contains(x1 + x2 + x3))
+                if (CheckColors((x1 + x2 + x3)) != Color.black)
                 {
-                    if (!Completed.Contains(Colors[i]))
+                    var color = CheckColors(x1 + x2 + x3);
+                    if (!Completed.Contains(color))
                     {
-                        Completed.Add(Colors[i]);
+                        Completed.Add(color);
                         Combinations.Add("x1 + x2 + x3");
                     }
                 }
@@ -134,6 +100,42 @@ public class ElementCreator : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    Color CheckColors(int i)
+    {
+        if (i >= 158 && i < 190)
+        {
+            return Color.red;
+        }
+        if (i >= 190 && i < 201)
+        {
+            return Color.clear;
+        }
+        if (i >= 201 && i < 210)
+        {
+            return Color.yellow;
+        }
+        if (i >= 210 && i < 238)
+        {
+            return Color.green;
+        }
+        if (i >= 238 && i < 245)
+        {
+            return Color.cyan;
+        }
+        if (i >= 245 && i < 264)
+        {
+            return Color.blue;
+        }
+        if (i >= 264 && i < 312)
+        {
+            return Color.magenta;
+        }
+        else
+        {
+            return Color.black;
         }
     }
 }
