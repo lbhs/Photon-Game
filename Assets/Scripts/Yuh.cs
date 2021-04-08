54a513f9daf63a25978ecd929bb3355067e49af4
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-04-08 13:38:17 -0700

    yuh

---
+++ Assets/Scripts/Yuh.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yuh : MonoBehaviour
+{
+    public void UrMom()
+    {
+
+    }
+
+    public void PickCombinations()
+    {
+
+    }
+
+    public void AssignColors()
+    {
+
+    }
+
+    public void FigureStuffOut()
+    {
+
+    }
+}
