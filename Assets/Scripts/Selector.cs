623f6018fc5e54897695d9712a579d823d452fa8
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-04-22 16:38:56 -0700

    select screen

---
+++ Assets/Scripts/Selector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            hit.collider.gameObject.GetComponent<Outline>().enabled = true;
+        }
+    }
+}
