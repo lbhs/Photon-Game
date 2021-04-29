f95640e631e87808a7209d9708a2fef42c53683d
smmccrty1 <70549146+smmccrty1@users.noreply.github.com>
2021-04-29 16:59:29 -0700

    stuff

--- Assets/Scripts/CardThing.cs
+++ Assets/Scripts/CardThing.cs
@@ -11,6 +11,18 @@ public class CardThing : MonoBehaviour
     public List<GameObject> FlippedCards;
     public GameObject LineManager;
 
+    public List<GameObject> Lines;
+    public GameObject electron;
+    public int CurrentLineNumber;
+    public initializeScreen initScreen;
+    public Element currentElement;
+    public List<GameObject> EligibleLines;
+
+    public void Start()
+    {
+        CurrentLineNumber = 0;
+    }
+
     public void FlipFirstCard()
     {
         var FirstCard = Cards[0];
@@ -20,12 +32,118 @@ public class CardThing : MonoBehaviour
         FirstCard.transform.position = new Vector3(pos.x, pos.y, 0 - pos.z);
         Cards.Remove(FirstCard);
         FlippedCards.Add(FirstCard);
+        EligibleLines = CheckLines();
     }
 
-    public int CurrentCard()
+    public List<GameObject> CheckLines()
     {
-        var Card = FlippedCards.Last();
-        var CardNumber = CardsForIndex.IndexOf(Card);
-        return CardNumber;
+        currentElement = initScreen.chosenElement;
+        var electronpos = electron.transform.position;
+        var currentcard = FlippedCards.Last();
+        var CardNumber = CardsForIndex.IndexOf(currentcard);
+        UnityEngine.Debug.Log("Card Number: " + CardNumber);
+        List<GameObject> ReturnLines = new List<GameObject>();
+        foreach (GameObject line in Lines)
+        {
+            int LineNumber = Lines.IndexOf(line);
+            var kJ2 = currentElement.kJValues[LineNumber];
+            var kJ1 = currentElement.kJValues[CurrentLineNumber];
+            var kJDiff = kJ2 - kJ1;
+            switch (CardNumber)
+            {
+                case 0:
+                    if (kJDiff <= 1000 && kJDiff > 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 1:
+                    if (LineNumber == 2)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 2:
+                    if (LineNumber == 1)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 3:
+                    if (kJDiff <= 700 && kJDiff >= 300)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 4:
+                    if (kJDiff < 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 5:
+                    if (LineNumber == 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 6:
+                    if (kJDiff <= 300 && kJDiff >= 170)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 7:
+                    if (kJDiff > 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 8:
+                    if (CurrentLineNumber == 1)
+                    {
+                        if (LineNumber == 0)
+                        {
+                            ReturnLines.Add(line);
+                        }
+                    }
+                    else
+                    {
+                        if (LineNumber == (CurrentLineNumber - 2))
+                        {
+                            ReturnLines.Add(line);
+                        }
+                    }
+                    break;
+                case 9:
+                    if (kJDiff <= 700 && kJDiff >= 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                case 10:
+                    if (kJDiff <= 800 && kJDiff >= 0)
+                    {
+                        ReturnLines.Add(line);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (ReturnLines.Count() > 0)
+        {
+            return ReturnLines;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void UpdateCurrentLine(int linenumber)
+    {
+        CurrentLineNumber = linenumber;
+        EligibleLines = null;
     }
 }
--- Assets/Scripts/LineManager.cs
+++ Assets/Scripts/LineManager.cs
@@ -17,13 +17,8 @@ public class LineManager : MonoBehaviour
     public GameObject Violet;
     public GameObject CardManager;
 
-    public initializeScreen initScreen;
-    public Element currentElement;
-    public int CurrentLineNumber;
-
     void Start()
     {
-        CurrentLineNumber = 0;
         
     }
     void Update()
@@ -37,165 +32,13 @@ public class LineManager : MonoBehaviour
             {
                 if (Lines.Contains(hit.collider.gameObject))
                 {
-                    UnityEngine.Debug.Log("hit line");
-                    var cardnumber = CardManager.GetComponent<CardThing>().CurrentCard();
-                    var kJ = CheckLines(cardnumber, hit.collider.gameObject);
-                    if (kJ != 0)
+                    if (CardManager.GetComponent<CardThing>().EligibleLines.Contains(hit.collider.gameObject))
                     {
                         electron.transform.position = hit.collider.gameObject.transform.position;
-                        CurrentLineNumber = Lines.IndexOf(hit.collider.gameObject);
-                        
-                        if (kJ >= -210 && kJ <= -158)
-                        {
-                            Debug.Log("Red");
-                            Red.SetActive(true);
-                        }
-                        if (kJ >= -310 && kJ <= -211)
-                        {
-                            Debug.Log("Orange");
-                            Orange.SetActive(true);
-                        }
-                        if (kJ >= -410 && kJ <= -311)
-                        {
-                            Debug.Log("Yellow");
-                            Yellow.SetActive(true);
-                        }
-                        if (kJ >= -510 && kJ <= -411)
-                        {
-                            Debug.Log("Green");
-                            Green.SetActive(true);
-                        }
-                        if (kJ >= -610 && kJ <= -511)
-                        {
-                            Debug.Log("Cyan");
-                            Blue.SetActive(true);
-                        }
-                        if (kJ >= -710 && kJ <= -611)
-                        {
-                            Debug.Log("Blue");
-                            Indigo.SetActive(true);
-                        }
-                        if (kJ >= -810 && kJ <= -711)
-                        {
-                            Debug.Log("Violet");
-                            Violet.SetActive(true);
-                        }
-                        if (kJ < -810)
-                        {
-                            Debug.Log("Ultraviolet");
-                        }
-                        if (kJ > -158 && kJ < 0)
-                        {
-                            Debug.Log("Infrared");
-                        }
+                        CardManager.GetComponent<CardThing>().UpdateCurrentLine(Lines.IndexOf(hit.collider.gameObject));
                     }
                 }
             }
-        }
-    }
-
-    public int CheckLines(int CardNumber, GameObject line)
-    {
-        currentElement = initScreen.chosenElement;
-        var electronpos = electron.transform.position;
-        UnityEngine.Debug.Log("Card Number: " + CardNumber);
-        bool Checkd = false;
-        int LineNumber = Lines.IndexOf(line);
-        UnityEngine.Debug.Log("LineNumber: " + LineNumber);
-        var kJ2 = currentElement.kJValues[LineNumber];
-        var kJ1 = currentElement.kJValues[CurrentLineNumber];
-        var kJDiff = kJ2 - kJ1;
-        UnityEngine.Debug.Log("kJ: " + kJDiff);
-        switch (CardNumber)
-        {
-            case 0:
-                if (kJDiff <= 1000 && kJDiff > 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 1:
-                if (LineNumber == 2)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 2:
-                if (LineNumber == 1)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 3:
-                if (kJDiff <= 700 && kJDiff >= 300)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 4:
-                if (kJDiff < 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 5:
-                if (LineNumber == 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 6:
-                if (kJDiff <= 300 && kJDiff >= 170)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 7:
-                if (kJDiff > 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 8:
-                if (CurrentLineNumber == 1)
-                {
-                    if (LineNumber == 0)
-                    {
-                        Checkd = true;
-                    }
-                }
-                else
-                {
-                    if (LineNumber == (CurrentLineNumber - 2))
-                    {
-                        Checkd = true;
-                    }
-                }
-                break;
-            case 9:
-                if (kJDiff <= 700 && kJDiff >= 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            case 10:
-                if (kJDiff <= 800 && kJDiff >= 0)
-                {
-                    Checkd = true;
-                }
-                break;
-            default:
-                break;
-        }
-        if (Checkd)
-        {
-            UnityEngine.Debug.Log("checkd");
-            return kJDiff;
-        }
-        else
-        {
-            UnityEngine.Debug.Log("failed");
-            return 0;
         }
     }
 }
